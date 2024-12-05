bf91f96528e54cddf378bb6ffdc06a194cdaa5c2
Monica Beate Tvedt <monica.beate.tvedt@gmail.com>
2024-12-05 13:24:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -11,3 +11,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+// run the server
+// node comments.js
+
+// open the browser and navigate to the URL
+// http://localhost:3000
+// You should see the message "Hello World" displayed in the browser
+
+// Now, let's add a new route to the web server
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page');
+});
+
+// Restart the server by pressing Ctrl + C in the terminal to stop the server and run the server again
+// Now, navigate to the URL http://localhost:3000/comments
+// You should see the message "This is the comments page" displayed in the browser
